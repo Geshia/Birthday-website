b75a0d3a44ca581f90c72915c058f888b3e28f9f
Geshia <richarduyiosa2019@gmail.com>
2022-09-22 09:21:40 -0700

    My birthday website

--- birthday.js
+++ birthday.js
@@ -10,7 +10,7 @@ var registration = function () {
 }
 
 
-setTimeout(registration, 2000);
+setTimeout(registration, 1000);
 
 // your custome placeholder goes here!
 var ph = 'Hey '+ yourName +'!'+ " wish Richard Happy Birthday ",
@@ -48,7 +48,7 @@ function printLetter(string, el) {
 		}
 	// use random speed to simulate
 	// 'human' typing
-	}, randDelay(50, 90));
+	}, randDelay(70, 100));
 }  
 
 // function to init animation
@@ -56,72 +56,52 @@ function placeholder() {
 	$(searchBar).attr("placeholder", "");
 	printLetter(ph, searchBar);
 }
-
 placeholder();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var introToBirthdayPresent = document.querySelector(".intro_to_birthdayPresent")
+// ============================================intro_to_Richard's__GAME+==========================================================
 var submitUserDetails2 = document.querySelector(".submit")
+var would_u_like_to_play = document.querySelector(".would_u_bgcontainer")
 
 submitUserDetails2.onclick = function () {
-    introToBirthdayPresent.style.display = "flex";
+	would_u_like_to_play.style.display = "flex";
     birthdayWish.style.display = "none";
 }
 
-var submitIntro = document.querySelector(".submit3")
-var gifts = document.querySelector(".gift_to_select")
-submitIntro.onclick = function () {
-    introToBirthdayPresent.style.display = "none";
-    gifts.style.display = "flex";
+// ============================================Richard's__GAME+==========================================================
+var Yes_play = document.querySelector(".would_u_like_to_play-Yes")
+var game_bgcontainer = document.querySelector(".game_bgcontainer")
+var game_bgcontainer2 = document.querySelector(".game_bgcontainer2")
+
+Yes_play.onclick = function () {
+	would_u_like_to_play.style.display = "none";
+    game_bgcontainer.style.display = "flex";
 }
 
-var gift1 = document.querySelector(".gift1")
-var gift2 = document.querySelector(".gift2")
+var correctAns1 = document.querySelector(".correctAns1")
+var wrongAns1 = document.querySelector(".wrongAns1")
 
-gift1.onclick = function () {
-    gift1.style.transform = "rotateY(180deg)";
-    gift2.style.display = "none";
-    gift1.style.width = "50%";
-} 
-
-gift2.onclick = function () {
-    gift2.style.transform = "rotateY(180deg)";
-    gift1.style.display = "none";
-    gift2.style.width = "50%";
+correctAns1.onclick = function () {
+	alert(yourName + ' Your answer is correct')
+	game_bgcontainer.style.display = "none";
+	game_bgcontainer2.style.display = "flex";
+}
+wrongAns1.onclick = function () {
+	alert(yourName + ' Your answer is wrong')
+	game_bgcontainer.style.display = "none";
+	game_bgcontainer2.style.display = "flex";
 }
 
+var correctAns1_2 = document.querySelector(".correctAns1_2")
+var wrongAns1_2 = document.querySelector(".wrongAns1_2")
+var game_bgcontainer3 = document.querySelector(".game_bgcontainer3")
 
-var id = null;
-function myMove() {
-  var elem = document.getElementById("imgAnimae");   
-  var pos = 0;
-  clearInterval(id);
-  id = setInterval(frame, 10);
-  function frame() {
-    if (pos == 400) {
-      clearInterval(id);
-    } else {
-      pos++; 
-      elem.style.top = pos + 'px'; 
-    //   elem.style.bottom = pos + 'px';
-    }
-  }
+correctAns1_2.onclick = function () {
+	alert(yourName + ' Your answer is correct')
+	game_bgcontainer2.style.display = "none";
+	game_bgcontainer3.style.display = "flex";
+}
+wrongAns1_2.onclick = function () {
+	alert(yourName + ' Your answer is wrong')
+	game_bgcontainer2.style.display = "none";
+	game_bgcontainer3.style.display = "flex";
 }
